f2aa2922e84963c09fd099bcdcc95352f5c514e8
sauce-Git <dadad1004@naver.com>
2024-11-25 11:19:07 +0900

    feat: update include and macro

--- solution.cpp
+++ solution.cpp
@@ -1,6 +1,10 @@
+/*#include <bits/stdc++.h>*/
 #include <iostream>
 
 using namespace std;
+
+#define rep(i, k, n) for (int i = k; i < (n); i++)
+#define repr(i, k, n) for (int i = k; i >= (n); i--)
 
 int main() {
   cout << "Hello, World!" << endl;
